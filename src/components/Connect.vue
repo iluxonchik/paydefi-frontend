6fb2a8c2d3d94120663735aadb8505b912b0ecf6
Illya Gerasymchuk <illya@iluxonchik.me>
2023-01-30 11:55:42 +0000

    feat: add Wallet connect component that connects to Metamask and notifies parent component about it

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,8 +1,42 @@
 <script setup>
 import Web3 from 'web3';
+import {computed, watch, onMounted, ref} from "vue";
 
+const emit = defineEmits(['walletConnected']);
+
+
+
+const isConnectedToWeb3 = ref(false);
+const accounts = ref(null);
+watch(accounts, async (newValue, oldValue) => {
+    if (newValue !== null) {
+        console.log("Connected to Wallet with: ", accounts);
+        emit('walletConnected', {
+            accounts: newValue,
+            web3: new Web3(window.ethereum),
+        });
+    }
+});
+
+function connectToWeb3() {
+    if (window.ethereum) {
+       // if Metamask is installed (it creates a window.ethereum object)
+
+        window.ethereum.request({
+            method: 'eth_requestAccounts'
+        }).then((connectedAccounts) => {
+            console.log("Connected accounts: ", connectedAccounts)
+            accounts.value = connectedAccounts;
+        });
+    }
+}
+
+onMounted(() => {
+    console.log("Loaded");
+    connectToWeb3();
+});
 
 </script>
 <template>
-    Connect
+    Requesting connection to your wallet...
 </template>
