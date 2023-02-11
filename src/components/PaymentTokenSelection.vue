5d0b1c7d5f3b60f3880f65baf308d3652617dbc4
Illya Gerasymchuk <illya@iluxonchik.me>
2023-02-11 11:43:57 +0000

    feat: display token payment selection for a PaymentRequest in a VSelect

---
+++ src/components/PaymentTokenSelection.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+import {computed, onMounted, reactive, ref} from "vue";
+
+const props = defineProps([
+    "paymentRequestId",
+    "web3",
+    "paymentRequestAddr",
+    "paymentRequestAbi",
+    "connectedAccountAddr"
+]);
+
+const web3 = ref(props.web3);
+const paymentRequestAddr = ref(props.paymentRequestAddr);
+const paymentRequestId = ref(props.paymentRequestId);
+const paymentRequestAbi = ref(props.paymentRequestAbi);
+const connectedAccountAddr = ref(props.connectedAccountAddr);
+const isPaymentRequestStatic = ref(false);
+const PaymentRequest = new web3.value.eth.Contract(
+    paymentRequestAbi.value,
+    paymentRequestAddr.value,
+);
+
+const selectedToken = ref(null);
+
+const acceptedTokenAmounts = ref([]);
+
+const tokenAmountsSelect = computed( () => {
+    return acceptedTokenAmounts.value.map(
+        (elem) => {
+            return {
+                title: elem.token + " | " + elem.tokenAmount,
+                token: elem.token
+            }
+    });
+});
+
+
+
+async function setIsPaymentRequestStatic() {
+    isPaymentRequestStatic.value = await PaymentRequest.methods.isTokenAmountStatic(paymentRequestId.value).call();
+}
+
+async function getStaticTokenAddressAmounts() {
+    acceptedTokenAmounts.value = await PaymentRequest.methods.getStaticTokenAmountInfos(paymentRequestId.value).call();
+
+}
+
+
+onMounted( () => {
+    setIsPaymentRequestStatic();
+    getStaticTokenAddressAmounts();
+
+})
+
+</script>
+
+<template>
+    <VContainer>
+    <VSelect
+        v-model="selectedToken"
+        :items="tokenAmountsSelect"
+        item-title="title"
+        item-value="token"
+        :hint="`Actual Name For Token ${selectedToken}`"
+        label="Select Payment Token"
+        persistent-hint
+        >
+
+    </VSelect>
+    </VContainer>
+
+
+</template>
